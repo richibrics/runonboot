d550be27d87bae1215c8339dfdb80f28ec5fb8dd
Riccardo Briccola <riccardo.briccola@mail.polimi.it>
2023-02-01 16:05:05 +0100

    Fix comment

--- runonboot/runner/PythonRunner.py
+++ runonboot/runner/PythonRunner.py
@@ -4,7 +4,7 @@ from runonboot.runner.Runner import Runner
 class PythonRunner(Runner):
     def __init__(self, name, python_arg: list):
         """ Runs on boot a python command, which is the parameter python_arg.
-            e.g. python_arg = ["-c", "print('hello world')"] will run the current python interpreter 
+            e.g. python_arg = ["-c", "\"print('hello world')\""] will run the current python interpreter 
             with the command "print('hello world')" as if called in shell in this way:
             {python_interpreter} -c "print('hello\ world')"
         """
