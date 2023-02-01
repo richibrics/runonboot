eca2b95ba27e62ff7a8b163120e900143bd8053d
Riccardo Briccola <riccardo.briccola@mail.polimi.it>
2023-02-01 09:56:17 +0100

    Improvements

--- examples/macos.py
+++ examples/macos.py
@@ -1,14 +1,17 @@
 import runonboot
-from runonboot.runner.Runner import Runner
+from runonboot.runner import PythonRunner
 import time
 
 if __name__ == "__main__":
-    r = Runner("test", "test")
+    r = PythonRunner("test", "/Users/mine/Downloads/PythonCode.py")
+    print("Installing the following command",r.getCommand())
+    print(runonboot.disclaimer())
     print("runner status: " + str(runonboot.isRunnerInstalled("test")))
     print("Installing runner...")
     runonboot.installRunner(r, user_only=False)
     print("runner status: " + str(runonboot.isRunnerInstalled("test")))
     time.sleep(5)
+    print("Removing runner...")
     runonboot.removeRunner("test")
     print("runner status: " + str(runonboot.isRunnerInstalled("test")))
     
