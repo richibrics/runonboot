03e37c7aa5f42862c52b83d35c5356a444fd64a7
Riccardo Briccola <riccardo.briccola@mail.polimi.it>
2023-02-01 16:02:39 +0100

    args

--- examples/macos.py
+++ examples/macos.py
@@ -3,12 +3,12 @@ from runonboot.runner import PythonRunner
 import time
 
 if __name__ == "__main__":
-    r = PythonRunner("test", "/Users/mine/Downloads/PythonCode.py")
-    print("Installing the following command",r.getCommand())
+    r = PythonRunner("test", ["/Users/mine/Downloads/PythonCode.py"])
+    print("Installing the following command"," ".join([r.getCommand()] + r.getArgs()))
     print(runonboot.disclaimer())
     print("runner status: " + str(runonboot.isRunnerInstalled("test")))
     print("Installing runner...")
-    runonboot.installRunner(r, user_only=False)
+    runonboot.installRunner(r, user_only=True)
     print("runner status: " + str(runonboot.isRunnerInstalled("test")))
     time.sleep(5)
     print("Removing runner...")
